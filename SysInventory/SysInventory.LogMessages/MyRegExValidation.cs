d49b6cd04eb344d0c155930540847dd117bd0c87
Gabriel Weibel <gabriel.weibel@hotmail.de>
2020-06-15 23:42:10 +0200

    Validation UnitTests

--- SysInventory/SysInventory.LogMessages/MyRegExValidation.cs
+++ SysInventory/SysInventory.LogMessages/MyRegExValidation.cs
@@ -20,13 +20,15 @@ namespace SysInventory.LogMessages
         public bool ValidateUrl(string url)
         {
             if (url == null) return false;
-            var regex = new Regex(@"(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?");
+            var regex = new Regex(@"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
             return regex.IsMatch(url);
         }
         public bool ValidatePassword(string password)
         {
             if (password == null) return false;
+            // Mit Sonderzeichen:
             var regex = new Regex(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$");
+            // ohne Sonderzeichen: ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,32}$
             return regex.IsMatch(password);
         }
     }
---
+++ SysInventory/SysInventory.Test/RegexTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using SysInventory.LogMessages;
+
+namespace SysInventory.Test
+{
+    [TestFixture]
+    public class RegexTests
+    {
+        [Test]
+        public void TestCustomerNumberValidation()
+        {
+            var validator = new MyRegExValidations();
+            var result = validator.ValidateCustomerNo("CU12345");
+            Assert.IsTrue(result);
+            result = validator.ValidateCustomerNo("CU123456");
+            Assert.IsFalse(result);
+            result = validator.ValidateCustomerNo("KU12345");
+            Assert.IsFalse(result);
+            result = validator.ValidateCustomerNo("");
+            Assert.IsFalse(result);
+            result = validator.ValidateCustomerNo(null);
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void TestEMailValidation()
+        {
+            var validator = new MyRegExValidations();
+            var result = validator.ValidateEMail("gabriel.weibel@hotmail.de");
+            Assert.IsTrue(result);
+            result = validator.ValidateEMail("gabriel.weibel@rey-automation.ch");
+            Assert.IsTrue(result);
+            result = validator.ValidateEMail("gabriel-weibel@rey-automation.ch");
+            Assert.IsTrue(result);
+            result = validator.ValidateEMail("gabriel-weibel_123@gmail1.com");
+            Assert.IsTrue(result);
+            result = validator.ValidateEMail("gabriel-weibel_123@gmail1.co.uk");
+            Assert.IsTrue(result);
+            result = validator.ValidateEMail("gabriel-weibel_123@gmail1..");
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void TestUrlValidation()
+        {
+            var validator = new MyRegExValidations();
+            var result = validator.ValidateUrl("www.google.com");
+            Assert.IsTrue(result);
+            result = validator.ValidateUrl("http://www.google.com");
+            Assert.IsTrue(result);
+            result = validator.ValidateUrl("https://www.google.com");
+            Assert.IsTrue(result);
+            result = validator.ValidateUrl("google.com");
+            Assert.IsTrue(result);
+            result = validator.ValidateUrl("https://policies.google.com");
+            Assert.IsTrue(result);
+            result = validator.ValidateUrl("https://policies.google.com/technologies/voice?hl=de&gl=ch");
+            Assert.IsTrue(result);
+            result = validator.ValidateUrl("https://www.policies.google.com/technologies/voice?hl=de&gl=ch");
+            Assert.IsTrue(result);
+            result = validator.ValidateUrl("ftp://www.policies.google.com/technologies/voice?hl=de&gl=ch");
+            Assert.IsFalse(result);
+            result = validator.ValidateUrl("asdf");
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void TestPasswordValidation()
+        {
+            var validator = new MyRegExValidations();
+            var result = validator.ValidatePassword("Test1234");
+            Assert.IsTrue(result);
+            result = validator.ValidatePassword("TeSt123467862");
+            Assert.IsTrue(result);
+            result = validator.ValidatePassword("TeSt123467862.");
+            Assert.IsTrue(result);
+            result = validator.ValidatePassword("Ta1");
+            Assert.IsFalse(result);
+            result = validator.ValidatePassword("asdfASDF");
+            Assert.IsFalse(result);
+            result = validator.ValidatePassword("123456789a");
+            Assert.IsFalse(result);
+        }
+    }
+}
