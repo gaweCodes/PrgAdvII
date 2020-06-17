dfc4c582d1586f1a41aef90674c1ab5cce384c1d
Gabriel Weibel <gabriel.weibel@hotmail.de>
2020-06-17 22:12:09 +0200

    added linq examples

---
+++ LINQ/Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Linq.ExtensionMethods;
+
+namespace Linq {
+    public class Program {
+        public static void Main(string[] args) {
+            ExpressionTrees.Examples.TestIntro();
+            LinqToXml.LinqToXml.Execute();
+
+            ExpressionTrees.Examples.TestPrintExpressionTree();
+            ExpressionTrees.Examples.TestBuildAnExpressionTree();
+
+            new LambdaExpressions.TypeInference.cs.Examples().TestRefOut();
+
+            new Linq.Initializers.Examples().TestCollectionInitializers();
+            new Linq.Initializers.Examples().TestObjectInitializers();
+            new Linq.Initializers.Examples().TestCombination();
+
+            new Linq.LambdaExpressions.AnonymousTypes.Examples().Test();
+
+            new Linq.LambdaExpressions.QueryExpressions.Examples().TestQuerySyntaxSelectMany();
+
+            new Examples().TestDeferredEvaluation();
+        }
+    }
+}
---
+++ LINQ/LinqToSql/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Data.Linq;  // Reference System.Data.Linq
+using System.Linq;
+using System.Data.Linq.Mapping;
+using System.Data.Linq.SqlClient;
+
+namespace LinqToSql{
+    class Program{
+        static void Main(string[] args) {
+            // https://docs.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-2017
+            var ctx = new DataContext(@"Data Source=.\symas;Initial Catalog=AdventureWorks2017;Trusted_Connection=True;");
+            ctx.Log = Console.Out;
+
+            var persons = ctx.GetTable<Person>();
+            var emails = ctx.GetTable<Email>();
+
+            // **** Queries with Extensionmethods ****
+            IQueryable<Person> persQuery = persons.Where(p => p.Firstname == "Rob");
+            var sql = persQuery.ToString();
+            var ex = persQuery.Expression;
+            foreach (var p in persQuery) {
+                Console.WriteLine($"ID={p.ID}, FirstName={p.Firstname}, LastName={p.Lastname}");
+            }
+
+            persQuery = persons.Where(p => p.Lastname.Contains("row"));
+            sql = persQuery.ToString();
+            ex = persQuery.Expression;
+            foreach (var p in persQuery) {
+                Console.WriteLine($"ID={p.ID}, FirstName={p.Firstname}, LastName={p.Lastname}");
+            }
+
+            //var query = persons.Join(emails, p => p.ID, e => e.PersonID, (p, e) => new {Person = p, Email = e}).Where(pe => pe.Person.Lastname.Contains("row")).Select(pe => pe.Email);
+            var query = persons.Join(emails, p => p.ID, e => e.PersonID, (p, e) => new {Person = p, Email = e}).Where(pe => pe.Person.Lastname.StartsWith("Walt")).Select(pe => pe.Email);
+            sql = query.ToString();
+            ex = query.Expression;
+            int personId = 0;
+            foreach (var e in query) {
+                Console.WriteLine($"ID={e.ID}, PersonID={e.PersonID}, Address={e.Address}");
+                personId = e.PersonID;
+            }
+
+
+            // **** Insert / Update ****
+            var mail = new Email() {PersonID = personId, Address = "t.kehl@symas-design.ch"};
+            emails.InsertOnSubmit(mail);
+            ctx.SubmitChanges();
+            Console.WriteLine($"ID: {mail.ID}");
+            mail.Address += "x";
+            ctx.SubmitChanges();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // **** Queries with Query Expressions ****
+            persQuery = from p in persons where p.Firstname == "Rob" select p;
+            sql = persQuery.ToString();
+
+            foreach (var p in persQuery) {
+                Console.WriteLine($"ID={p.ID}, FirstName={p.Firstname}, LastName={p.Lastname}");
+            }
+
+            persQuery = from p in persons where SqlMethods.Like(p.Lastname, "Walt%") select p;
+            foreach (var p in persQuery) {
+                Console.WriteLine($"ID={p.ID}, FirstName={p.Firstname}, LastName={p.Lastname}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+    [Table(Name = "Person.Person")] // needs: using System.Data.Linq.Mapping;
+    public class Person{
+        [Column(IsPrimaryKey = true, Name = "BusinessEntityID")]
+        public int ID { get; set; }
+
+        [Column(Name = "FirstName")]
+        public string Firstname { get; set; }
+
+        [Column(Name = "LastName")]
+        public string Lastname { get; set; }
+    }
+
+
+    [Table(Name = "Person.EmailAddress")] // needs: using System.Data.Linq.Mapping;
+    public class Email {
+        [Column(IsPrimaryKey = true, Name = "BusinessEntityID")]
+        public int PersonID { get; set; }
+      
+        [Column(IsPrimaryKey = true, Name = "EmailAddressID", IsDbGenerated = true)]
+        public int ID { get; set; }
+
+        [Column(Name = "EmailAddress")]
+        public string Address { get; set; }
+
+        [Column(Name = "RowGuid", IsDbGenerated = true)]
+        public Guid RowGuid { get; set; }
+
+        [Column(Name = "ModifiedDate", IsDbGenerated = true)]
+        public DateTime ModifiedAt { get; set; }
+    }
+}
+
+
+
+
